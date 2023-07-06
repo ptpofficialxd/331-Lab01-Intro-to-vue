bc7fe213f5ab1157cc15d6a429da07e4d15e683b
ptpofficialxd <aekkungzaza@gmail.com>
2023-07-07 00:13:39 +0700

    add ProductDisplay.js

---
+++ components/ProductDetails.js
@@ -0,0 +1,14 @@
+const productDetails = {
+    template: `
+    <ul>
+        <li v-for="detail in details">{{detail}}</li>
+    </ul>
+    `,
+    props: {
+        details: Array
+    },
+    setup(props) {
+        const details = ref(props.details)
+        return { details }
+    }
+}
